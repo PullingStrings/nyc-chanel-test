935e4cd1169f6c1e632ec692353fd9de9d7c1d94
Tito <drammatik@gmail.com>
2023-12-07 19:04:59 +0000

    menu finished section started

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 import '../styles/style.scss'
-import javascriptLogo from '../assets/javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from '../js/counter'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.getElementById('menu__toggle');
+    var menuBtn = document.querySelector('.menu__btn');
+    var menuBox = document.querySelector('.menu__box');
+    var closeBox = document.querySelector('.menu-item-close');
 
-setupCounter(document.querySelector('#counter'))
+    menuToggle.addEventListener('change', function() {
+        if (this.checked) {
+            // Menu opened
+             menuBtn.classList.add('open');
+             menuBox.style.display = 'block';
+        } else {
+            // Menu closed
+            menuBtn.classList.remove('open');
+            menuBox.style.display = 'none';
+        }
+    });
+});
+
