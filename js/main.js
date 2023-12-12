14650312e899160e39c361d22e0f0d251a27a32b
Tito <drammatik@gmail.com>
2023-12-12 16:58:01 +0000

    adding to vercal

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import '../styles/style.scss'
 
 document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.getElementById('menu__toggle');
-    var menuBtn = document.querySelector('.menu__btn');
-    var menuBox = document.querySelector('.menu__box');
-    var closeBox = document.querySelector('.menu-item-close');
+    let menuToggle = document.getElementById('menu__toggle');
+    let menuBtn = document.querySelector('.menu__btn');
+    let menuBox = document.querySelector('.menu__box');
+    let closeBox = document.querySelector('.menu-item-close');
 
     menuToggle.addEventListener('change', function() {
         if (this.checked) {
@@ -17,5 +17,61 @@ document.addEventListener('DOMContentLoaded', function() {
             menuBox.style.display = 'none';
         }
     });
+
+    document.querySelector(".menu__item.introduction")
+
+    document.querySelector(".menu__item.introduction").onclick = function() {
+    document.getElementById('introduction').scrollIntoView({
+        behavior: 'smooth'
+    });
+
+};
+
+const goToSection = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth', block: "start", inline: "start" });
+};
+
+const menuItems = document.querySelectorAll('.menu__item');
+const sectionMapping = {
+    'introduction': 'introduction',
+    'gabrielle_chanel': 'gabrielle_chanel',
+    'the_iconic': 'the_iconic',
+    'savoir_faire': 'savoir_faire',
+    'the_necklace': 'the_necklace'
+};
+
+menuItems.forEach((listItem) => {
+    let sectionTitles = listItem.innerText;
+    listItem.addEventListener('click', (e) => {
+        e.preventDefault();
+        const classSuffix = listItem.classList[1];
+        const sectionId = sectionMapping[classSuffix];
+
+        if (sectionId) {
+            document.querySelector(".nav-menu-icon > p").innerText = sectionTitles;
+            goToSection(sectionId);
+        }
+    });
+});
+
+// const navMenuContainer = document.querySelector(".item-nav-menu-container");
+// const chanelBanner = document.querySelector(".chanel-main-banner");
+// const screenWidth = window.innerWidth;
+//  window.addEventListener('scroll', function() {
+
+//         // Get the distance from the top of the document to the element
+//         const distanceFromTop = chanelBanner.getBoundingClientRect().top;
+
+//         if (distanceFromTop >= 51 && screenWidth <= 768) {
+//             // Element is at the top or above the top of the viewport
+//             navMenuContainer.style.height = '300px';
+//             document.querySelector(".nav-menu-icon > p").innerText = "Menu"
+
+//         } else {
+//             navMenuContainer.style.height = '46px';
+//         }
+//     });
+
+
 });
 
